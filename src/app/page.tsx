2d3fed686946b36c502dad1a9308b14e142c962b
andyzhengbaige <91546129+andyzhengbaige@users.noreply.github.com>
2024-03-27 20:04:49 +1100

    Added auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,23 @@ import { api } from "~/trpc/server";
 
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+  // returns information on current user, null if signed out
   const user = await currentUser();
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
 
-        <UserButton />
-        {!user && <SignInButton />}
-        {user && <SignOutButton />}
+  return (
+    <main className="min-h-screen flex-col bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="flex container bg-white/10 mx-auto rounded-full items-center">
+        <div className="justify-center w-15 h-15 p-3">
+          <UserButton />
+        </div>
+        <div className="rounded-full bg-white/10 px-5 py-3 font-semibold transition hover:bg-white/20 h-10">
+          <span className="align-top md:align-top">
+            {!user && <SignInButton />}
+            {user && <SignOutButton />}
+          </span>
+        </div>
+      </div>
+      <div className="container items-center content-center box-border h-280 w-320 p-4 sm mx-auto object-center justify-center flex flex-col">
         <CrudShowcase />
       </div>
     </main>
@@ -27,7 +35,7 @@ async function CrudShowcase() {
   const latestPost = await api.post.getLatest();
 
   return (
-    <div className="w-full max-w-xs">
+    <div className="w-full max-w-xs flex flex-col">
       {latestPost ? (
         <p className="truncate">Your most recent post: {latestPost.name}</p>
       ) : (
